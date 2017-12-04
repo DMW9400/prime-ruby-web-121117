785dfab08068e8a8891fae59c3e7a402baa3b617
Matthew Welch <dmw9400@gmail.com>
2017-12-04 19:36:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+
+def prime? (number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  elsif number%2 == 0 or number%3 == 0
+    return false
+  end
+  i = 5
+  while i*i < number
+    if number%i == 0 or number%(i+2) == 0
+    return false
+    i = i + 6
+    return true
+  end
+end
+end
