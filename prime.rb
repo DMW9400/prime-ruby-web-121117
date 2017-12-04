d4d17b94f871db766ae340866257aa5885c118ad
Matthew Welch <dmw9400@gmail.com>
2017-12-04 19:44:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -4,12 +4,12 @@ def prime? (number)
     return false
   elsif number <= 3
     return true
-  elsif number%2 == 0 or number%3 == 0
+  elsif number%2 == 0 | number%3 == 0
     return false
   end
   i = 5
   while i*i < number
-    if number%i == 0 or number%(i+2) == 0
+    if number%i == 0 | number%(i+2) == 0
     return false
     i = i + 6
     return true
